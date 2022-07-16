5f5125440e49a691a03477a4ae49d4a71401ba62
Nicholas <nicholas.nmalinda@gmail.com>
2022-07-16 15:43:13 +0300

    Lab Ccomplete

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,30 @@
+// import React, { useState } from "react";
+// import ShoppingList from "./ShoppingList";
+// import Header from "./Header";
+// import itemData from "../data/items";
+
+// function App() {
+//   const [items, setItems] = useState(itemData);
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+
+//   function handleDarkModeClick() {
+//     setIsDarkMode((isDarkMode) => !isDarkMode);
+//   }
+
+//   function handleItemFormSubmit(newItem){
+//     setItems([...newItem, newItem]);
+//   }
+
+//   return (
+//     <div className={"App " + (isDarkMode ? "dark" : "light")}>
+//       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+//       <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
@@ -11,12 +38,17 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
 
 export default App;
+
